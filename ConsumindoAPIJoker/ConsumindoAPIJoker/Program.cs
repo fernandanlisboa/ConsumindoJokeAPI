30bc297c0eb8d71c6dbc628f7a9863f55e348c4c
fernandanlisboa <fernandanlisboa@gmail.com>
2020-09-25 18:15:38 -0300

    adicionando menu de opções

--- ConsumindoAPIJoker/ConsumindoAPIJoker/Program.cs
+++ ConsumindoAPIJoker/ConsumindoAPIJoker/Program.cs
@@ -6,7 +6,36 @@ namespace ConsumindoAPIJoker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\n\t\t\tBem vindo(a) ao gerador de piadas aleatórias!!!\n");
+            while (true)
+            {
+                Console.WriteLine("Digite sempre 0 para encerrar!\n");
+                PrintaMenuCategorias();
+                int categoria = Convert.ToInt32(Console.ReadLine());
+                if (categoria == 0)
+                    break;
+                PrintaMenuTipo();
+                int tipo = Convert.ToInt32(Console.ReadLine());
+                if (tipo == 0)
+                    break;
+            }
+            
+        }
+        private static void PrintaMenuCategorias()
+        {
+            Console.WriteLine("\nEscolha a categoria de piada de sua preferência:\n");
+            Console.WriteLine("[1] Programação\n" +
+                "[2] Diversos\n" +
+                "[3] Pesado\n" +
+                "[4] Trocadilho\n" +
+                "[5] Todas\n");
+        }
+        private static void PrintaMenuTipo()
+        {
+            Console.WriteLine("\nEscolha o tipo de piada:\n");
+            Console.WriteLine("[1] Com pergunta\n" +
+                "[2] Sem pergunta\n" +
+                "[3] Todos\n");
         }
     }
 }
