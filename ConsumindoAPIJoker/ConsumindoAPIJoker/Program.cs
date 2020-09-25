228f69ff923f19e1dc20693ed3c794f863dc86eb
fernandanlisboa <fernandanlisboa@gmail.com>
2020-09-25 17:46:32 -0300

    initial commit

---
+++ ConsumindoAPIJoker/ConsumindoAPIJoker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsumindoAPIJoker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
