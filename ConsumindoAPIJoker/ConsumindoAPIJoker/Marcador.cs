f947d7c4335731f2f8ba4709719d6aabf4f2cafb
fernandanlisboa <fernandanlisboa@gmail.com>
2020-09-25 18:44:29 -0300

    criação e estruturação das classes

---
+++ ConsumindoAPIJoker/ConsumindoAPIJoker/Marcador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsumindoAPIJoker
+{
+    public class Marcador
+    {
+        public bool nsfw { get; set; }
+        public bool religious { get; set; }
+        public bool political { get; set; }
+        public bool racist { get; set; }
+        public bool sexist { get; set; }
+    }
+}
---
+++ ConsumindoAPIJoker/ConsumindoAPIJoker/Piada.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsumindoAPIJoker
+{
+    public class Piada
+    {
+        public int formatVersion { get; set; }
+        public string category { get; set; }
+        public string type { get; set; }
+        public string joke { get; set; }
+        public Marcador flags { get; set; }
+        public string lang { get; set; }
+    }
+}
