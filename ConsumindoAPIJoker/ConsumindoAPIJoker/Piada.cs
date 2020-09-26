05956ccb3c388563b01e23a09854dc4f670fec63
fernandanlisboa <fernandanlisboa@gmail.com>
2020-09-25 21:28:22 -0300

    atualizando estrutura da classe Piada

--- ConsumindoAPIJoker/ConsumindoAPIJoker/Piada.cs
+++ ConsumindoAPIJoker/ConsumindoAPIJoker/Piada.cs
@@ -6,11 +6,14 @@ namespace ConsumindoAPIJoker
 {
     public class Piada
     {
-        public int formatVersion { get; set; }
+        public bool error { get; set; }
         public string category { get; set; }
         public string type { get; set; }
         public string joke { get; set; }
+        public string setup { get; set; }
+        public string delivery { get; set; }
         public Marcador flags { get; set; }
+        public int id { get; set; }
         public string lang { get; set; }
     }
 }
